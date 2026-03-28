c102271b199827c20b0632bb9fe6b7535a3287e2
RxxM8 <ram.mouli.k@gmail.com>
2026-03-28 20:32:26 +0530

    fix3

--- 3_next_greatest_element.c
+++ 3_next_greatest_element.c
@@ -3,8 +3,7 @@
 int stack[100];
 int top = -1;
 
-void push(int x)
-{
+void push(int x){
     if (top == 99)
     {
         printf("Stack Overflow\n");
@@ -13,27 +12,23 @@ void push(int x)
     stack[++top] = x;
 }
 
-int pop()
-{
+int pop(){
     if (top == -1)
         return -1;
     return stack[top--];
 }
 
-int peek()
-{
+int peek(){
     if (top == -1)
         return -1;
     return stack[top];
 }
 
-int isEmpty()
-{
+int isEmpty(){
     return top == -1;
 }
 
-int main()
-{
+int main(){
     int n;
     printf("Enter number of elements: ");
     scanf("%d", &n);
@@ -41,15 +36,14 @@ int main()
     int data[100], result[100];
 
     printf("Enter %d elements: ", n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
         scanf("%d", &data[i]);
+    }
 
-   
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        while (!isEmpty() && peek() <= data[i])
+    for (int i = n - 1; i >= 0; i--){
+        while (!isEmpty() && peek() <= data[i]){
             pop();
+        }
 
         result[i] = peek();   
 
@@ -57,8 +51,8 @@ int main()
     }
 
     printf("\nOutput:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
         printf("%d -> %d\n", data[i], result[i]);
-
+    }
     return 0;
 }
